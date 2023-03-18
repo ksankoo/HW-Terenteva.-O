2db165e248608863988bf4a74230b719fc490d65
ksankoo <113919893+ksankoo@users.noreply.github.com>
2023-03-18 14:02:05 +0300

    Add files via upload

---
+++ 9/training/operators.js
@@ -0,0 +1,29 @@
+let a =  '10';
+let b = '2';
+
+// alert(Number(a) + Number(b));
+
+// alert(String(5));
+
+alert(Boolean("0"));
+
+// let c = a + b;
+// let c = a - b;
+// let c = a * b;
+// let c = a / b;
+// let c = a % b;
+// let c = a ** b;
+
+let d = '2';
+let e = '2';
+
+// alert(d + e);
+
+d += 5;
+d = d + 5;
+
+let message;
+message = 'Привет!';
+
+console.log(message); 
+
---
+++ 9/training/start.js
@@ -0,0 +1,32 @@
+let message ='Привет!';
+let message2;
+message2 = 5;
+
+let x = 5;
+let y = x;
+x=3;
+
+// alert(x*y);
+
+var age=30;
+
+const CONST = 100;
+
+let n = 123;
+    n = 12.3;
+
+let name = 'Окси';
+let name2 = "Ося";
+let name3 = `Оксана`;
+
+let message_to = `Привет, ${name}`;
+let message_to2 = 'Привет, ' + name2;
+
+alert (message_to2);
+
+let yes = true;
+let no = false;
+
+let a = undefined;
+
+let b = null;
